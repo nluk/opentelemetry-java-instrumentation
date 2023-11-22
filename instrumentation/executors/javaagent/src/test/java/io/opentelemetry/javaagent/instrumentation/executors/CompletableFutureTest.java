db45e5637e6bfd1d4196936d6271e48fa0327164
Łukasz Nowak <lukasz.nowak@idemia.com>
2023-11-22 12:05:58 +0100

    Added test for span propagation with shared completable future

--- instrumentation/executors/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/executors/CompletableFutureTest.java
+++ instrumentation/executors/javaagent/src/test/java/io/opentelemetry/javaagent/instrumentation/executors/CompletableFutureTest.java
@@ -10,10 +10,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.instrumentation.testing.junit.AgentInstrumentationExtension;
 import io.opentelemetry.instrumentation.testing.junit.InstrumentationExtension;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import io.opentelemetry.sdk.trace.data.SpanData;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -195,6 +201,98 @@ class CompletableFutureTest {
                 span -> span.hasName("parent").hasKind(SpanKind.INTERNAL).hasNoParent(),
                 span ->
                     span.hasName("child").hasKind(SpanKind.INTERNAL).hasParent(trace.getSpan(0))));
+  }
+
+  @Test
+  void sharedCompletableFuture(){
+
+    ExecutorService tokenFetchingExecutor = Executors.newSingleThreadExecutor();
+
+    Supplier<String> longTokenFetchProcess = () ->
+      testing.runWithSpan("tokenFetch", () -> {
+        try {
+          Thread.sleep(5000L);
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+          throw new RuntimeException(e);
+        }
+        return "token";
+      });
+
+    // Originally this CompletableFuture would be a result of java.net.http.HttpClient::sendAsync
+    // A supplier to ensure the call will be executed only if needed
+    Supplier<CompletableFuture<String>> singleTokenFetch =  () -> CompletableFuture.supplyAsync(
+        longTokenFetchProcess,
+        tokenFetchingExecutor
+    );
+
+    ConcurrentHashMap<String, CompletableFuture<String>> fetchStorage = new ConcurrentHashMap<>();
+
+    ExecutorService controller1Executor = Executors.newSingleThreadExecutor();
+    ExecutorService controller2Executor = Executors.newSingleThreadExecutor();
+
+    CountDownLatch sync = new CountDownLatch(2);
+
+    CompletableFuture<String> controller1 =
+        testing.runWithSpan(
+            "controller1-start",
+            () -> CompletableFuture.supplyAsync(() -> {
+                  sync.countDown();
+                  try {
+                    sync.await();
+                  } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                  }
+                  return "controller1-start";
+                }  , controller1Executor)
+                .thenCompose(result -> fetchStorage.computeIfAbsent("COMMON_TOKEN", _ignored -> singleTokenFetch.get()))
+                .thenApplyAsync(token -> {
+                  testing.runWithSpan("controller1-end", () -> {});
+                  return "controller1-end";
+                }, controller1Executor)
+        );
+
+    CompletableFuture<String> controller2 =
+        testing.runWithSpan(
+            "controller2-start",
+            () -> CompletableFuture.supplyAsync(() -> {
+                  sync.countDown();
+                  try {
+                    sync.await();
+                  } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                  }
+                  return "controller2-start";
+                }  , controller2Executor)
+                .thenCompose(result -> fetchStorage.computeIfAbsent("COMMON_TOKEN", _ignored -> singleTokenFetch.get()))
+                .thenApplyAsync(token -> {
+                  testing.runWithSpan("controller2-end", () -> {});
+                  return "controller2-end";
+                }, controller2Executor)
+        );
+
+    CompletableFuture.allOf(controller1, controller2).join();
+
+    assertThat(controller1.join()).isEqualTo("controller1-end");
+    assertThat(controller2.join()).isEqualTo("controller2-end");
+
+    List<List<SpanData>> traces = testing.waitForTraces(2);
+
+    Set<String> c1Spans = traces.get(0)
+        .stream()
+        .map(SpanData::getName)
+        .collect(Collectors.toSet());
+    Set<String> c2Spans = traces.get(1)
+        .stream()
+        .map(SpanData::getName)
+        .collect(Collectors.toSet());
+
+    assertThat("tokenFetch").satisfiesAnyOf(
+        span -> assertThat(c1Spans).contains(span),
+        span -> assertThat(c2Spans).contains(span)
+    );
+    assertThat(c2Spans).contains("controller2-start", "controller2-end");
+    assertThat(c1Spans).contains("controller1-start", "controller1-end");
   }
 
   static final class AppendingSupplier implements Supplier<String> {
